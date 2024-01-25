370478dd3f11bf6d5699d3aebb543ee55b94bfc8
Thomas Dambrin <thomas.dambrin@gmail.com>
2024-01-24 22:27:05 -0500

    [TEST] tests for commands

--- tests/conftest.py
+++ tests/conftest.py
@@ -13,8 +13,8 @@ PACKAGE_ROOT = Path(__file__).parent.parent
 TEST_CONF = dotenv_values(PACKAGE_ROOT / "sf_git.test.conf")
 
 TESTING_FOLDER = Path(__file__).parent.absolute()
-REPO_ROOT_PATH = Path(TEST_CONF['SNOWFLAKE_VERSIONING_REPO']).absolute()
-REPO_DATA_PATH = Path(TEST_CONF['WORKSHEETS_PATH']).absolute()
+REPO_ROOT_PATH = Path(TEST_CONF["SNOWFLAKE_VERSIONING_REPO"]).absolute()
+REPO_DATA_PATH = Path(TEST_CONF["WORKSHEETS_PATH"]).absolute()
 
 
 @pytest.fixture(scope="session", name="testing_folder")
@@ -22,16 +22,20 @@ def testing_folder():
     return TESTING_FOLDER
 
 
-@pytest.fixture(autouse=True)
-def mock_dotenv_path(monkeypatch):
-    import sf_git
-    monkeypatch.setattr(sf_git, "DOTENV_PATH", REPO_ROOT_PATH / 'sf_git.test.conf')
+@pytest.fixture(scope="session", name="fixture_dotenv_path")
+def dotenv_path():
+    return REPO_ROOT_PATH / "sf_git.test.conf"
 
 
-@pytest.fixture(scope='session', autouse=True)
+@pytest.fixture(scope="session", name="repo_root_path")
+def repo_root_path():
+    return REPO_ROOT_PATH
+
+
+@pytest.fixture(scope="session", autouse=True)
 def setup_and_teardown():
     """
-        Setup test envs and teardown
+    Setup test envs and teardown
     """
 
     # ---- SETUP -----
@@ -39,7 +43,9 @@ def setup_and_teardown():
     if REPO_ROOT_PATH.is_dir():
         shutil.rmtree(REPO_ROOT_PATH)
 
-    shutil.copytree(TESTING_FOLDER / "data", REPO_DATA_PATH, dirs_exist_ok=True)
+    shutil.copytree(
+        TESTING_FOLDER / "data", REPO_DATA_PATH, dirs_exist_ok=True
+    )
     shutil.copy(PACKAGE_ROOT / "sf_git.test.conf", REPO_ROOT_PATH)
 
     # ---- RUN TESTS -----
@@ -49,15 +55,16 @@ def setup_and_teardown():
     shutil.rmtree(REPO_ROOT_PATH)
 
 
-@pytest.fixture(autouse=True, name='test_config')
+@pytest.fixture(autouse=True, name="test_config")
 def mock_config(monkeypatch):
     import sf_git.config as config
 
     global_config = Config(
         repo_path=REPO_ROOT_PATH,
         worksheets_path=REPO_DATA_PATH,
-        sf_account_id=TEST_CONF['SF_ACCOUNT_ID'],
-        sf_login_name=TEST_CONF['SF_LOGIN_NAME'],
+        sf_account_id=TEST_CONF["SF_ACCOUNT_ID"],
+        sf_login_name=TEST_CONF["SF_LOGIN_NAME"],
+        sf_pwd=TEST_CONF["SF_PWD"],
     )
     monkeypatch.setattr(config, "GLOBAL_CONFIG", global_config)
 
@@ -67,12 +74,14 @@ def mock_config(monkeypatch):
 @pytest.fixture(scope="session", name="repo")
 def repo() -> Repo:
     """
-        Create a git repository with sample data and history.
+    Create a git repository with sample data and history.
     """
 
     author = Actor("An author", "author@example.com")
     committer = Actor("A committer", "committer@example.com")
-    commit_date = datetime.datetime(2024, 1, 16, tzinfo=ZoneInfo("Europe/Paris"))
+    commit_date = datetime.datetime(
+        2024, 1, 16, tzinfo=ZoneInfo("Europe/Paris")
+    )
 
     # Init repo
     repo = Repo.init(REPO_ROOT_PATH)
@@ -90,23 +99,23 @@ def repo() -> Repo:
     return repo
 
 
-@pytest.fixture(scope="session", name='auth_context')
+@pytest.fixture(scope="session", name="auth_context")
 def auth_context() -> sf_git.models.AuthenticationContext:
     import sf_git.config as config
 
     context = sf_git.models.AuthenticationContext(
         app_server_url="https://test_snowflake.com",
         account_url="https://account.test_snowflake.com",
-        account='fake.snowflake.account',
+        account="fake.snowflake.account",
         account_name=config.GLOBAL_CONFIG.sf_account_id,
-        client_id='fake',
-        main_app_url='https://test_snowflake.com',
-        master_token='fake',
-        organization_id='fake.organization',
-        region='west-europe',
-        snowsight_token={'fake': 'fake'},
+        client_id="fake",
+        main_app_url="https://test_snowflake.com",
+        master_token="fake",
+        organization_id="fake.organization",
+        region="west-europe",
+        snowsight_token={"fake": "fake"},
         login_name=config.GLOBAL_CONFIG.sf_login_name,
-        username=config.GLOBAL_CONFIG.sf_login_name
+        username=config.GLOBAL_CONFIG.sf_login_name,
     )
 
     return context
